1f578961740c2f038e59d209a56c9cb89faf8fb6
SakethParvataneni <sakethparvataneni@gmail.com>
2023-06-05 12:39:56 -0400

    This is my first commit for the code

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,3 +8,25 @@ resource "aws_s3_bucket" "saketh-indv-movielens-raw-dataset-tf-test" {
   bucket = "saketh-indv-movielens-raw-dataset-tf-test"
   acl    = "private"
 }
+
+resource "aws_cloudwatch_event_rule" "my_event_rule" {
+  name        = "my-event-rule"
+  description = "Trigger Lambda function on specific events"
+  event_pattern = <<EOF
+{
+  "source": [
+    "aws.ec2"
+  ],
+  "detail-type": [
+    "EC2 Instance State-change Notification"
+  ]
+}
+EOF
+}
+
+# Create the EventBridge target to invoke the Lambda function
+resource "aws_cloudwatch_event_target" "my_event_target" {
+  rule      = aws_cloudwatch_event_rule.my_event_rule.name
+  target_id = "my-event-target"
+  arn       = aws_lambda_function.lambda-src-raw.arn
+}
